2eec8aa2754c835c7e743abd6d2619a8353bfbd6
chukwuernest <chukwu17mc@gmail.com>
2024-07-18 09:27:26 -0400

    Side-Effects,Custom Hooks,Fragments

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 import TodoList from './TodoList'
 import AddTodoForm from '../AddTodoForm'
 
 function App() {
-  // const [newTodo, setNewTodo] = useState('')
-
-  const [todoList, setTodoList] = useState([])
+  const [newTodo, setNewTodo] = useState('')
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo])
     console.log(newTodo)
   }
+  const useSemiPersistentState = () => {
+    let savedTodoList = localStorage.getItem('savedTodoList')
+    return savedTodoList ? JSON.parse(savedTodoList) : []
+  }
+
+  const [todoList, setTodoList] = useState(useSemiPersistentState)
+
+  useEffect(() => {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList))
+    console.log(todoList)
+  }, [todoList])
+
   return (
     <>
       <h1>Todo List</h1>
